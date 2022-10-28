192b5620c48609597ac2e8d932a116a97b0df8b3
moon3000 <howie.jia@gmail.com>
2022-10-29 03:08:49 +0800

    10/28

---
+++ FeiBot/Program.cs
@@ -0,0 +1,24 @@
+﻿using Flandre.Core;
+using Flandre.Adapters.Konata;
+using Konata.Core.Common;
+using Flandre.Core.Common;
+using Flandre.Core.Messaging;
+
+var app = new FlandreApp();
+
+var config = new KonataAdapterConfig();
+config.Bots.Add(new KonataBotConfig
+{
+    KeyStore = new BotKeyStore("1477336749", "Pippi301")
+});
+
+app
+    .UseKonataAdapter(config)
+    .Use(new ExamplePlugin())
+    .Start();
+
+class ExamplePlugin : Plugin
+{
+    public override void OnMessageReceived(MessageContext ctx)
+        => ctx.Bot.SendMessage(ctx.Message);
+}
